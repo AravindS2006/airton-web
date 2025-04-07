8c6e1b3d6068ca42e7cf3fd4fbe0b1a391009e71
AravindS2006 <aravindselvan2006@gmail.com>
2025-04-07 23:10:50 +0530

    final commit

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     domains: ['placehold.co'], // Allow images from placehold.co
   },
+  // Ignore deprecation warnings during build
+  webpack: (config) => {
+    // Ignore deprecation warnings
+    config.ignoreWarnings = [
+      { module: /node_modules\/@nextui-org/ },
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
